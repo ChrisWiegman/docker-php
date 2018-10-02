beabbd2b25e76ff60cf455d07ffde07e580b1779
Chris Wiegman <chris@wiegman.us>
2018-10-02 10:39:39 -0400

    Remove PHP 7.1

--- 01-Core/7.1/xdebug-listen-on.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sh -c  "grep -q -F 'xdebug.remote_autostart = 1' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini || echo 'xdebug.remote_autostart = 1' >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini"
-ldconfig
